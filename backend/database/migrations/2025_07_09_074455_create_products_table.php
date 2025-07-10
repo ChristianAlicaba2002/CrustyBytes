80830f644c281c0a89db7e32c780c15001398c73
Christian <christiandave120702@gmail.com>
2025-07-10 10:18:05 +0800

    Added Products Migrations Table

---
+++ backend/database/migrations/2025_07_09_074455_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id')->primary()->autoIncrement(false);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('category', ['pizza','drink','dessert'])->default('pizza');
+            $table->enum('size', ['small', 'medium', 'large'])->default('medium');
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

84f6a12d05f69291443efb6394aeb60bbe53fd7b
Christian <christiandave120702@gmail.com>
2025-07-11 20:22:33 +0800

    Add a post request from backend and pass the user data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,64 @@
 import { getAuth, signOut } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import type { TUser } from "../Types";
 
 export default function Home() {
     const navigate = useNavigate();
     const auth = getAuth();
+    const [user, setUser] = useState<TUser>({
+        uId: "",
+        name: "",
+        email: "",
+        password: "",
+    })
 
     useEffect(() => {
         try {
-            const isUserSignEnd = auth.onAuthStateChanged((auth) => {
-                if (!auth) {
+            const isUserSignEnd = auth.onAuthStateChanged((userAuth) => {
+                if (!userAuth) {
                     navigate('/')
                 }
+
+                setUser((prev) => ({
+                    ...prev,
+                    uId: userAuth?.uid || "",
+                    name: userAuth?.displayName || "",
+                    email: userAuth?.email || "",
+                    password: "",
+
+                }))
             })
             return () => isUserSignEnd()
         } catch (error) {
             console.log('something is error', error)
         }
     }, [auth, navigate])
+
+
+    useEffect(() => {
+        const postUserData = async () => {
+            try {
+                const response = await fetch("http://localhost:8000/api/users", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({...user})
+                });
+
+                const dataUser = await response.json()
+                if (dataUser) {
+                    console.log(dataUser)
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        postUserData()
+    }, [user])
+
+
 
     const signOutUser = async () => {
         try {

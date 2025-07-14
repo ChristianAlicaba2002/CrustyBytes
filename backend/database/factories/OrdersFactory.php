0e547e4838d0e9102a9ef4b957b457340e65fce5
Christian <christiandave120702@gmail.com>
2025-07-14 21:09:29 +0800

    Add a Order Factory fot labeling in Feature Test

---
+++ backend/database/factories/OrdersFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Orders;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Orders>
+ */
+class OrdersFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Orders::class;
+    public function definition(): array
+    {
+        return [
+            'user_id' => $this->faker->word(),
+            'total_price' => $this->faker->randomFloat(2,8),
+            'status' => $this->faker->randomElement(['pending', 'processing', 'out_for_delivery', 'cancelled']),
+            'payment_method' => $this->faker->word(),
+        ];
+    }
+}

6d9103704f44ed7db41ad2989d7e41c1600828c2
Christian <christiandave120702@gmail.com>
2025-07-14 17:13:02 +0800

    add a primaryKey named uId

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $table = 'users';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'uId';
     protected $fillable = [
         'uId',
         'name',

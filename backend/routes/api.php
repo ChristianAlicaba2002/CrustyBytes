0e2ba266505f235ce33475276dcdd9a722302e74
Christian <christiandave120702@gmail.com>
2025-07-14 21:06:40 +0800

    Add a API Order

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ProductAPIController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProductAPIController;
+use App\Http\Controllers\API\Order\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,10 +14,11 @@ Route::get('/storage/{imageName}', function ($imageName) {
     return response()->file(public_path('images/' . $imageName));
 });
 
-
 //User Routes
 Route::post('users',[UserController::class , 'createUser']);
 
-
 //Product Routes
 Route::resource('/products', ProductAPIController::class);
+
+//Order Routes
+Route::resource('/order', OrderController::class);

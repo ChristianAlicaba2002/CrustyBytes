92c0d43dbb36076c75bb28d75fa1f451be7a454d
Christian <christiandave120702@gmail.com>
2025-07-14 17:13:48 +0800

    change the primary key into uId

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id')->primary()->autoIncrement(true);
-            $table->string('uId');
+            $table->string('uId')->primary()->autoIncrement(false);
             $table->string('name');
             $table->string('phone_number')->unique()->nullable();
             $table->string('city')->nullable();

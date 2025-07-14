47724012e2924ac94ad74ed2856414f5fa50aa75
Christian <christiandave120702@gmail.com>
2025-07-14 21:09:08 +0800

    remove the primary key

--- backend/app/Models/Orders.php
+++ backend/app/Models/Orders.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
+    use HasFactory;
     
     protected $table = 'orders';
-    protected $primaryKey = 'id';
     protected $fillable = [
         'user_id',
         'total_price',

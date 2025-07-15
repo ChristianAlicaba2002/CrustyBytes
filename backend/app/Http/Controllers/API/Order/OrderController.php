c125690f92bd054e016d7b400400b0f5f1e3995a
Christian <christiandave120702@gmail.com>
2025-07-15 21:29:00 +0800

    Add a Order Controller

--- backend/app/Http/Controllers/API/Order/OrderController.php
+++ backend/app/Http/Controllers/API/Order/OrderController.php
@@ -7,9 +7,17 @@ use App\Models\Orders;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Application\Order\OrderHandler;
 
 class OrderController extends Controller
 {
+
+    public function __construct(private OrderHandler $orderHandler)
+    {
+        $this->orderHandler = $orderHandler;
+    }
+
+
     /**
      * Display a listing of the resource.
      */
@@ -32,22 +40,45 @@ class OrderController extends Controller
     public function store(Request $request)
     {
         // Correct validation
-        $Validator = $request->validate([
+        $Validator = Validator::make($request->all(), [
             'user_id' => 'required|string',
+            'name' => 'required|string',
+            'phone_number' => 'required|string',
+            'address' => 'required|string',
             'total_price' => 'required|numeric',
             'payment_method' => 'required|string',
         ]);
+
+        if ($Validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $Validator->errors()
+            ]);
+        }
 
         $user_order = User::where('uId', $request->user_id)->first();
 
         if ($user_order) {
             // Correct order creation
-            $order = Orders::create([
-                'user_id' => $request->user_id,
-                'total_price' => $request->total_price,
-                'status' => 'pending',
-                'payment_method' => $request->payment_method,
-            ]);
+            // $order = Orders::create([
+            //     'user_id' => $request->user_id,
+            //     'name' => $request->name,
+            //     'phone_number' => $request->phone_number,
+            //     'address' => $request->address,
+            //     'total_price' => $request->total_price,
+            //     'status' => 'pending',
+            //     'payment_method' => $request->payment_method,
+            // ]);
+
+            $order = $this->orderHandler->create(
+                $request->user_id,
+                $request->name,
+                $request->phone_number,
+                $request->address,
+                $request->total_price,
+                'pending',
+                $request->payment_method,
+            );
 
             return response()->json([
                 'status' => true,

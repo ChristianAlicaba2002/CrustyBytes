d022731ba3d8cf8e8786ed49ec15c755b6277935
Christian <christiandave120702@gmail.com>
2025-07-15 21:30:25 +0800

    Update the table Order

--- backend/database/migrations/2025_07_09_074506_create_orders_table.php
+++ backend/database/migrations/2025_07_09_074506_create_orders_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('address');
             $table->decimal('total_price', 8, 2);
             $table->enum('status', ['pending', 'processing', 'out_for_delivery', 'cancelled'])->default('pending');
             $table->string('payment_method');

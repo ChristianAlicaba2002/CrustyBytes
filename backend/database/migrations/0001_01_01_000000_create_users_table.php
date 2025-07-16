60144985de18e1df573a930627546fbb4102020b
Christian <christiandave120702@gmail.com>
2025-07-16 17:16:23 +0800

    Minimal update  on users migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('uId')->primary()->autoIncrement(false);
             $table->string('name');
-            $table->string('phone_number')->unique()->nullable();
+            $table->string('phone_number')->nullable();
             $table->string('city')->nullable();
             $table->string('barangay')->nullable();
             $table->string('purok')->nullable();
